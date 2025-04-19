be61f97baa69ef2af85ea83c68903736ab301e25
Liam Dobbelaere <liam.dobbelaere@diekeure.be>
2025-04-19 15:38:45 +0200

    Add STMP

--- index.js
+++ index.js
@@ -9,11 +9,13 @@ const path = require("path");
 const oth = require("./oth");
 const streamlist = require("./streamlist")(http);
 const loctrac = require("./loctrac");
+const sillyTavernMP = require("./silly-tavern-mp");
 
 app.use("/", express.static(path.join(__dirname, "public")));
 app.use("/oth", oth.app);
 app.use("/streamlist", streamlist.app);
 app.use("/loctrac", loctrac.app);
+app.use("/silly-tavern-mp", sillyTavernMP.app);
 
 http.listen(PORT, () => {
   console.log(`listening on *:${PORT}`);
---
+++ silly-tavern-mp/index.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+let chatHistory = [];
+let queuedMessages = [];
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.set('trust proxy', true);
+app.use("/", express.static(path.join(__dirname, "public")));
+
+// disable Access-Control-Allow-Origin
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+
+// serve static files from the public directory
+app.use(express.static('public'));
+
+app.post('/set-chat', (req, res) => {
+    chatHistory = req.body;
+
+    res.send('Chat history received and stored successfully');
+});
+
+app.get('/get-chat', (req, res) => {
+    res.json(chatHistory);
+});
+
+app.post('/queue-message', (req, res) => {
+    queuedMessages.push(req.body);
+    console.log('Queued message:', req.body);
+
+    res.send('Message queued successfully');
+});
+
+app.get('/queued-messages', (req, res) => {
+    res.json(queuedMessages);
+    queuedMessages = [];
+});
+
+console.log("Silly Tavern MP loaded");
+
+module.exports = {
+  app,
+};
